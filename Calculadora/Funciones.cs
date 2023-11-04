e937518d073b2191ce60ca7c9c8e7a5335ca5dc2
Jair <jairestebanca@gmail.com>
2023-11-04 00:16:03 -0500

    Se implementaron las funciones root (raíz cuadrada) y power (potencia)!

--- Calculadora/Funciones.cs
+++ Calculadora/Funciones.cs
@@ -23,7 +23,7 @@ namespace Calculadora
             return num1 * num2;
         }
 
-        public double Division(double num1, double num2)
+        public double Division(int num1, int num2)
         {
             double division_num1 = (double) num1;
             double division_num2 = (double) num2;
@@ -37,6 +37,16 @@ namespace Calculadora
             {
                 return division_num1 / division_num2;
             }
+        }
+
+        public double Root(int num1)
+        {
+            return Math.Sqrt(num1);
+        }
+
+        public double Power(int num1_base, int num2_exp)
+        {
+            return Math.Pow(num1_base, num2_exp);
         }
     }
 }
